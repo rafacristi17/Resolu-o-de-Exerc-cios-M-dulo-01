e6a9cbd3a23bd3dad30ab84fe426cf82998cd826
rafacristi17 <rafaelacristine@live.com>
2024-04-21 22:53:42 -0300

    feat: resposta questao 04

---
+++ resposta04.js
@@ -0,0 +1,20 @@
+/*4.Escreva um algoritmo para ler as notas das duas avaliações de um aluno no semestre,
+calcular e escrever a média semestral e a seguinte mensagem: ‘PARABÉNS! Você foi
+aprovado’, somente se o aluno foi aprovado (considere 6.0 a nota mínima para
+aprovação).*/
+
+
+let prompt= require("prompt-sync")();
+
+let ava1 = parseFloat(prompt("Digite a nota da primeira avaliação : "));
+let ava2 = parseFloat(prompt("Digite a nota da segunda avaliação : "));
+
+const media = (ava1+ava2)/2;
+
+if(media >= 6.0 && media <= 10){
+    console.log("PARABÉNS! Você foi aprovado! Sua média ficou em : ", media);
+}else if(media <= 5.0 && media <= 0.0){
+    console.log("Que pena, você foi Reprovado! Sua média ficou em : ", media);
+}else{
+    console.log("Digite um número válido");
+}
