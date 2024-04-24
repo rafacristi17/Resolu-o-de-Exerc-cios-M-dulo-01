ff831ff8b2ec98e4c2a7ca1b2440859150173f95
rafacristi17 <rafaelacristine@live.com>
2024-04-23 22:44:17 -0300

    feat: resposta da questao 08

---
+++ resposta08.js
@@ -0,0 +1,11 @@
+/*
+8. Escreva um algoritmo para ler 2 valores (considere que não serão lidos valores iguais)
+e escreve-los em ordem crescente.
+*/
+let prompt= require("prompt-sync")();
+
+
+let numeros= [7,6]
+numeros.sort()
+    console.log("Os numeros em ordem crescente ficou : "+numeros)
+
