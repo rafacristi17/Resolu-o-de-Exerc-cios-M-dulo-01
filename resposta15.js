82cfcfd7c4d842fdf29e0b66074dd08c5f1a136b
rafacristi17 <rafaelacristine@live.com>
2024-04-28 22:05:54 -0300

    feat: resposta da questao 15

--- resposta15.js
+++ resposta15.js
@@ -3,3 +3,24 @@
 que o usuário digite o número 0. Fazer a média ponderada desses números e pesos
 respectivos.
 */
+
+let prompt= require("prompt-sync")();
+
+
+let somaPonderada = 0;
+let somaPesos = 0;
+let numero, peso;
+
+console.log("Digite um número decimal e seu peso. Digite 0 para o número para parar.");
+
+while(true){
+    numero = parseFloat(prompt("Digite um número: "));
+    if(numero === 0) break;
+    peso = parseFloat(prompt("Digite o peso do número: "));
+    somaPonderada += numero * peso;
+    somaPesos += peso;
+}
+
+let mediaPonderada = somaPonderada / somaPesos;
+
+console.log(`A média ponderada dos números digitados é ${mediaPonderada}.`);
