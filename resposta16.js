02680b1a7fae87e875e959fd3360e6b05298a377
rafacristi17 <rafaelacristine@live.com>
2024-04-28 23:35:28 -0300

    feat: resposta da questao 16

--- resposta16.js
+++ resposta16.js
@@ -2,3 +2,20 @@
 16. Escreva um algoritmo para imprimir os 50 primeiros número primos maior que 100.
 Obs.: Número primo é aquele divisível somente por 1 e ele mesmo.
 */
+
+function isPrime(num) {
+    for(let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
+}
+
+let count = 0;
+let num = 101;
+
+while(count < 50) {
+    if(isPrime(num)) {
+        console.log(num);
+        count++;
+    }
+    num++;
+}
